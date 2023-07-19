455eed4343dddbf220b354c619f722257b85230c
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2023-07-20 00:30:24 +0600

    Add files via upload

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,16 +1,27 @@
-#include<bits/stdc++.h>
+#include <stdio.h>
+
 using namespace std;
 
-int main() {
-    int a,b,c;
-    cin >> a >> b >> c;
-    for (int i = 0; i <= c / a; i++) {
-        if ((c - i * a) % b == 0) {
-            cout << "YES";
-            return 0;
-        }
+int main()
+{
+    int n, s, count[5]= {0};
+    scanf("%d", &n);
+    while (n--)
+    {
+        scanf("%d", &s);
+        count[s] += 1;
     }
-    cout << "NO";
- 
+    int total = count[4] + count[3] + count[2] / 2;
+    count[1] -= count[3];
+    if (count[2] % 2 == 1)
+    {
+        total += 1;
+        count[1] -= 2;
+    }
+    if (count[1] > 0)
+    {
+        total += (count[1] + 3) / 4;
+    }
+    printf("%d\n", total);
     return 0;
 }
