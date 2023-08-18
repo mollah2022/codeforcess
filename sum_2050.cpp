d601fdbcb863a96814d9e303712f92aa79387ee0
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2023-08-18 23:45:46 +0600

    Add files via upload

---
+++ sum_2050.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define mp(x,y) make_pair(x,y)
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define tst int t;cin>>t;while(t--)
+#define srt(v) sort(v.begin(),v.end());
+#define rsrt(v) sort(v.rbegin(),v.rend());
+#define sajib ios::sync_with_stdio(false);cin.tie(0);
+#define rvs(v) reverse(v.begin(),v.end());
+#define F first
+#define S second
+#define MOD 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define PI 2*acos(0.0)
+#define pii pair<int,int>
+#define fr(i,a,b) for(int i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define ld long double
+#define nl '\n'
+const int N = 1e5;
+using namespace std;
+#ifndef ONLINE_JUDGE
+#else
+#define dbg(x...)
+#endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_rand(int l, int r)
+{
+   return uniform_int_distribution<int>(l, r) (rng);
+}
+
+int32_t main(){
+    
+    sajib
+    tst{
+      ll n; cin >> n;
+      int ans = 0;
+      while(n>=2050){
+         ans++;
+         ll x = 2050;
+         while(x<=n){
+            x*=10;
+         }
+         x/=10;
+         n = n-x;
+      }
+      if(n!=0) 
+         cout << -1 << endl;
+      else
+         cout << ans << endl;
+    }
+   return 0;
+}
---
+++ vector_erase.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define mp(x,y) make_pair(x,y)
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define tst int t;cin>>t;while(t--)
+#define srt(v) sort(v.begin(),v.end());
+#define rsrt(v) sort(v.rbegin(),v.rend());
+#define sajib ios::sync_with_stdio(false);cin.tie(0);
+#define rvs(v) reverse(v.begin(),v.end());
+#define F first
+#define S second
+#define MOD 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define PI 2*acos(0.0)
+#define pii pair<int,int>
+#define fr(i,a,b) for(int i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define ld long double
+#define nl '\n'
+const int N = 1e5;
+using namespace std;
+#ifndef ONLINE_JUDGE
+#else
+#define dbg(x...)
+#endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_rand(int l, int r)
+{
+   return uniform_int_distribution<int>(l, r) (rng);
+}
+
+int32_t main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin >> n;
+    vector <int> v1;
+    for(int i=1; i<=n; i++){
+      int y; cin >> y;
+      v1.push_back(y);
+    }
+    int p; cin >> p;
+    int x,z; cin >> x >> z;
+    v1.erase(v1.begin()+(p-1));
+    v1.erase(v1.begin()+(x-1),v1.begin()+(z-1));
+    int k = v1.size();
+
+    cout << k << endl;
+    for(auto l:v1){
+      cout << l << " ";
+    }
+    cout << endl;
+   return 0;
+}
