a0e7aaa9fcd806b1adf4784fd49d33d2f20f0499
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-02-07 19:25:51 +0600

    Add files via upload

---
+++ 1351a.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll   long long
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0);
+#define tst  int t; cin >> t; while(t--)
+#define yes  cout << "YES\n";
+#define no   cout << "NO\n";
+
+int32_t main(){
+           FAST
+              tst{
+                    ll a,b; cin >> a >> b;
+                    ll sum = (a+b);
+                    cout << sum << endl;
+              }   
+     return 0;
+}
